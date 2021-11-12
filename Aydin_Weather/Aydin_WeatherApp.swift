315410e775e6a6364b5d4c3565fb0bfb80c754db
Aydin Battal <abattal35@gmail.com>
2021-11-11 21:48:56 -0500

    Initial Commit

---
+++ Aydin_Weather/Aydin_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aydin_WeatherApp.swift
+//  Aydin_Weather
+//
+//  Created by Aydin Battal on 2021-11-11.
+//
+
+import SwiftUI
+
+@main
+struct Aydin_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aydin_Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aydin_Weather
+//
+//  Created by Aydin Battal on 2021-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
